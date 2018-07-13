2e00145774a84f8e77539991cd49dc2ba65917c4
marco-oliveira <marco-oliveira@live.com>
2018-07-12 21:19:19 -0300

    Refatorando codigo para usar CDI

--- src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
+++ src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
@@ -1,32 +1,30 @@
 package br.com.caelum.vraptor.controller;
 
-import java.util.List;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 
 import br.com.caelum.vraptor.Controller;
-import br.com.caelum.vraptor.Delete;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.dao.ProdutoDao;
 import br.com.caelum.vraptor.model.Produto;
-import br.com.caelum.vraptor.util.JPAUtil;
 import br.com.caelum.vraptor.view.Results;
 
 @Controller
 public class ProdutoController {
 	
 	private final Result result;
+	private ProdutoDao dao;
 	
 	public ProdutoController() {
-		this(null);
+		this(null, null);
 	}
 	
 	@Inject
-	public ProdutoController(Result result) {
+	public ProdutoController(Result result, ProdutoDao produtoDao) {
 		this.result = result;
+		this.dao = produtoDao;
 	}
 	
 	@Get
@@ -36,21 +34,16 @@ public class ProdutoController {
 	
 	@Get
 	public void lista() {
-		EntityManager em = JPAUtil.criaEntityManager();
-		ProdutoDao dao = new ProdutoDao(em);
 		result.include("produtoList", dao.lista());
 	}
 	
 	@Get
 	public void listaEmXml() {
-		EntityManager em = JPAUtil.criaEntityManager();
-		ProdutoDao dao = new ProdutoDao(em);
 		result.use(Results.xml()).from(dao.lista()).serialize();
 	}
+	
 	@Get
 	public void listaEmJson() {
-		EntityManager em = JPAUtil.criaEntityManager();
-		ProdutoDao dao = new ProdutoDao(em);
 		result.use(Results.json()).from(dao.lista()).serialize();
 	}
 	
@@ -61,21 +54,13 @@ public class ProdutoController {
 	
 	@Post
 	public void adiciona(Produto produto) {
-		EntityManager em = JPAUtil.criaEntityManager();
-		ProdutoDao produtoDao = new ProdutoDao(em);
-		em.getTransaction().begin();
-		produtoDao.adiciona(produto);
-		em.getTransaction().commit();
+		dao.adiciona(produto);
 		result.include("mensagem", "Produto Cadastrado com sucesso!");
 		result.redirectTo(this).lista();
 	}
 	
 	@Get
 	public void remove(Produto produto) {
-		EntityManager em = JPAUtil.criaEntityManager();
-		ProdutoDao produtoDao = new ProdutoDao(em);
-		em.getTransaction().begin();
-		produtoDao.remove(produto);
-		em.getTransaction().commit();
+		dao.remove(produto);
 	}
 }
